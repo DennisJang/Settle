968b54ff03ce1ad350505786630178ea64db9659
DennisJang <sean15243@gmail.com>
2026-03-29 13:04:43 +0000

    1차 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-  import './i18n/index';
-  // src/main.tsx
-import './styles/theme.css';  // 시맨틱 토큰
-import './styles/index.css';  // Tailwind + 기타
-
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import './i18n/index';
+import './styles/index.css';      // 이 안에서 theme.css를 @import 함
   createRoot(document.getElementById("root")!).render(<App />);
   
